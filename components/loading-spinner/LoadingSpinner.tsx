a3f5d64991086ddaa02ee89be5869240252c39fd
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-12-04 16:00:09 +0100

    feat: LoadingSpinner comp

---
+++ components/loading-spinner/LoadingSpinner.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import { StyledLoadingDonut } from './LoadingSpinner.Styled';
+
+const LoadingSpinner: FC<{}> = () => {
+	return <StyledLoadingDonut />;
+};
+
+export default LoadingSpinner;
